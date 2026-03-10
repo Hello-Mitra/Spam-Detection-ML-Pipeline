dc31e8fefd40b25c4f496351fa8001c5808af46a
Mitra <arijit.mitra22@gmail.com>
2026-03-10 23:01:56 +0530

    pipeline configured using DVC, with params

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -28,9 +28,8 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
-if not logger.handlers:
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 def transform_text(text):
     """
