b67ed979a26b5eee9fbeac7cb1b6110842468e9a
Mitra <arijit.mitra22@gmail.com>
2026-03-11 10:59:00 +0530

    exp tracking using DVC done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -6,7 +6,7 @@ import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
 import yaml
-# from dvclive import Live
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
@@ -92,7 +92,7 @@ def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
             'auc': auc
         }
         logger.debug('Model evaluation metrics calculated')
-        return metrics_dict
+        return metrics_dict, y_pred, y_pred_proba
     except Exception as e:
         logger.error('Error during model evaluation: %s', e)
         raise
@@ -119,15 +119,16 @@ def main():
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
 
-        metrics = evaluate_model(clf, X_test, y_test)
+        metrics, y_pred, y_pred_proba = evaluate_model(clf, X_test, y_test)
 
-        # # Experiment tracking using dvclive
-        # with Live(save_dvc_exp=True) as live:
-        #     live.log_metric('accuracy', accuracy_score(y_test, y_test))
-        #     live.log_metric('precision', precision_score(y_test, y_test))
-        #     live.log_metric('recall', recall_score(y_test, y_test))
+        # Experiment tracking using dvclive
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_pred))
+            live.log_metric('precision', precision_score(y_test, y_pred))
+            live.log_metric('recall', recall_score(y_test, y_pred))
+            live.log_metric('auc', roc_auc_score(y_test, y_pred_proba))
 
-        #     live.log_params(params)
+            live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
